40efa60446fab4ce052ed01d991d0ceb14123483
Ritesh Bafna <Riteshbafna025@gmail.com>
2026-09-06 18:36:39 +0530

    Create Room

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,7 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "../app/api/[[...slugs]]/route";
 
-export const client = treaty<App>("https://sneak-talk.vercel.app/").api;
+const apiOrigin =
+	typeof window === "undefined" ? "http://localhost:3000" : window.location.origin;
+
+export const client = treaty<App>(apiOrigin).api;
